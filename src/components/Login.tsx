08f153b7938d52057cf9f5f94c241d09c1bf6e16
Hannah Beattie <hannahdbeattie@gmail.com>
2023-01-25 21:50:40 +1300

    redirect on login

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,53 +1,37 @@
 import { getAppAuth, uiConfig } from '@/firebaseConfig'
-import {
-	Box,
-	Button,
-	HStack,
-	Link,
-	Modal,
-	ModalBody,
-	ModalCloseButton,
-	ModalContent,
-	ModalHeader,
-	ModalOverlay,
-	Spacer,
-	Text,
-	Toast,
-	useDisclosure,
-} from '@chakra-ui/react'
-import { useState } from 'react'
+import { Button, HStack, Spacer, Text } from '@chakra-ui/react'
+import { useRouter } from 'next/router'
 import { useAuthState } from 'react-firebase-hooks/auth'
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth'
 
 export default function Login() {
+	const router = useRouter()
 	const auth = getAppAuth()
 	const [user, loading, error] = useAuthState(auth)
 	const name = user?.displayName
 	const text = name ? `Hello ${name}` : 'sign in'
-	const { isOpen, onOpen, onClose } = useDisclosure()
-	const [signedOut, setSignedOut] = useState(false)
 
-	if (!user && !loading) {
+	if (loading) {
+		return null
+	}
+
+	if (!user) {
 		return <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={auth} />
 	}
 
-	if (user && !signedOut) {
-		return (
-			<HStack flex={'1'} px={4}>
-				<Text color={'Black'}>{text}</Text>
-				<Spacer />
-				<Link href={'/'}>
-					<Button
-						colorScheme={'red'}
-						onClick={() => {
-							auth.signOut()
-							setSignedOut(true)
-						}}
-					>
-						Sign-out
-					</Button>
-				</Link>
-			</HStack>
-		)
-	}
+	return (
+		<HStack flex={'1'} px={4}>
+			<Text color={'Black'}>{text}</Text>
+			<Spacer />
+			<Button
+				colorScheme={'red'}
+				onClick={() => {
+					router.push('/')
+					auth.signOut()
+				}}
+			>
+				Sign-out
+			</Button>
+		</HStack>
+	)
 }
