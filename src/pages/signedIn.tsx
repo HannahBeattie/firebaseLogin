7d8023e389ecc057b429005912fbd55459e96240
Hannah Beattie <hannahdbeattie@gmail.com>
2023-01-26 12:56:30 +1300

    display avatar of logged in person

--- src/pages/signedIn.tsx
+++ src/pages/signedIn.tsx
@@ -1,15 +1,18 @@
 import { useAuthContext } from '@/lib/AuthContext'
-import { Box, Heading, Image, VStack } from '@chakra-ui/react'
+import { Avatar, Box, Heading, HStack, Image, VStack } from '@chakra-ui/react'
 
 export default function SignedIn() {
 	const { user } = useAuthContext()
 	console.log('user is:', user?.displayName)
 	const name = user?.displayName ?? ''
+	const url = user?.photoURL ? user?.photoURL : ''
 	return (
 		<Box>
-			<VStack flex={1} py={8}>
-				<Heading> Hello {name}</Heading>
-				{user?.photoURL && <Image src={user.photoURL} alt='Your profile picture' />}
+			<VStack flex={1} py={8} spacing={8}>
+				{user && <Avatar size={'100'} src={url} />}
+				<HStack>
+					<Heading> Hello {name}</Heading>
+				</HStack>
 			</VStack>
 		</Box>
 	)
